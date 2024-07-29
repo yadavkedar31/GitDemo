f3c8bca5578f9726f691dfcb6fd24bd3a2b6e9ce
kedaryadav <yadav.kedar31@gmail.com>
2024-07-29 17:25:30 +0530

    Kedar tested

---
+++ TEstNG8/src/Testnglearn/Test1.java
@@ -0,0 +1,58 @@
+package Testnglearn;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void ked() {
+		// TODO Auto-generated method stub
+		System.out.println("kedar");
+		System.out.println("kedar22");
+		System.out.println("kedar33");
+		System.out.println("kedar44");
+	}
+
+	@Test(groups= {"smoke"})
+	public void testsecong() {
+		System.out.println("Ydav");
+
+	}
+
+	@BeforeMethod
+	private void TEste2() {
+		// TODO Auto-generated method stub
+
+		System.out.println("************");
+
+	}
+
+	@AfterMethod
+	private void TEe2() {
+		// TODO Auto-generated method stub
+
+		System.out.println("$$$$$$$$$$$$$$$$$$$$");
+
+	}
+	
+	@AfterClass
+	private void AfterClass() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Clas test 1 end");
+
+	}
+	
+	
+	@BeforeClass
+	private void Beforeclass() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class Test 1 Start");
+
+	}
+}
